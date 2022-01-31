5da661bedf17efcbb90a6c70a5e6f22eacd70a16
Miroslav Savel <miroslav.savel@gmail.com>
2022-01-31 12:37:24 +0100

    add event listener on button and fire event after click

--- flipper.js
+++ flipper.js
@@ -1,1 +1,16 @@
-alert( 'Color flipper!' );
+//alert( 'Color flipper!' );
+
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+//here we select button by html ID
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+// and add event listener, we will listen to click event
+btn.addEventListener('click', function(){
+    console.log(document.body);
+    const randomNumber = 2;
+    //we will get random number between 0-3
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
