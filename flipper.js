5ea85248c1e558181de3422adec2ccfd0b382474
Miroslav Savel <miroslav.savel@gmail.com>
2022-02-01 14:42:45 +0100

    change the background color based on pseudorandomly choosen color from array

--- flipper.js
+++ flipper.js
@@ -9,8 +9,15 @@ const color = document.querySelector(".color");
 // and add event listener, we will listen to click event
 btn.addEventListener('click', function(){
     console.log(document.body);
-    const randomNumber = 2;
+    // const randomNumber = 2;
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
     //we will get random number between 0-3
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 });
+
+function getRandomNumber(){
+    //it takes the number of elements of array colors
+    return Math.floor(Math.random() * colors.length);
+}
